7a6257816c0e4c9b0b774a932803ee93f334a118
npnam2k3 <nguyenphuongnam284@gmail.com>
2024-07-29 11:06:05 +0700

    crud product

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,10 @@
 const userRouter = require("./user");
+const productRouter = require("./product");
 const { notFound, errorHandler } = require("../middlewares/errorHandler");
 
 const initRoutes = (app) => {
   app.use("/api/user", userRouter);
+  app.use("/api/product", productRouter);
 
   // neu khong trung voi url nao thi se chay vao ham notFound
   app.use(notFound);
---
+++ server/routes/product.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const productController = require("../controllers/product");
+const { verifyAccessToken, isAdmin } = require("../middlewares/verifyToken");
+
+router.post("/", [verifyAccessToken, isAdmin], productController.createProduct);
+router.get("/", productController.getAllProduct);
+router.put(
+  "/:productId",
+  [verifyAccessToken, isAdmin],
+  productController.updateProduct
+);
+router.delete(
+  "/:productId",
+  [verifyAccessToken, isAdmin],
+  productController.deleteProduct
+);
+
+// có params thì để cuối cùng tránh bị nhầm giữa các route
+router.get("/:productId", productController.getProduct);
+
+module.exports = router;
